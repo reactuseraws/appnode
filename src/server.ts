ab6c4cb2f3c918d5c84c660135264f7d9f22b3ba
reactuseraws <sal.marcelonardi@hotmail.com>
2021-10-02 13:30:54 -0300

    adding files

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,7 @@
-//import express, { request, response } from "express";
-import express from "express";
-import bodyParser from "body-parser";
-
-//var bodyParser = require('body-parser')
-//import "./DataBase";
-
 /*
-CONEXÃO COM BANCO DE DADOS
+AUTHOR: MARCELO F V NARDI
+DATA..: 09/2021
 
-INICIO
-*/
-
-const mysql = require('mysql');
-
-const con = mysql.createConnection(
-  { host     : "cad.testeaws.sa-east-1.rds.amazonaws.com",
-    user     : "xyz",
-    password : "zyzxyz",
-    port     : "3306"
-  });
-
-con.connect(function(err) {
-if (err)
-   { 
-    console.error('AWS Database connection failed: ' + err.stack);
-    return
-   }
-else
-   {
-    console.log('Connected to AWS database.');
-    return
-   };
-
-});
-
-const app = express();
-
-var jsonParser = bodyParser.json()
-/*
 GET     => BUSCAR UMA INFORMAÇÃO NA API
 POST    => INSERIR UMA INFORMAÇÃO NA API
 PUT     => ALTERAR UMA INFORMAÇÃO NA API
@@ -47,54 +11,110 @@ PATCH   => ALTERAR UMA INFORMAÇÃO ESPECÍFICA
 
 /*
 TIPOS DE PARÂMETROS
-
 ROUTES PARAMS => FAZEM PARTE DA ROTA  | LOCALHOST:3000/produtos/100200
-QUERY PARAMS  => FAZER UM FILTRO |  LOCALHOST:3000/produtos?name=teclados&descricao=sem fio -- não obrigatórios
+QUERY PARAMS  => FAZER UM FILTRO      |  LOCALHOST:3000/produtos?name=teclados&descricao=sem fio -- não obrigatórios
 BODY PARAMS   => MÉDITO POST/PUT/PATCH { "name": "teclado", "descricao": "sem fio"}
-
 */
+
+import express from "express";
+import bodyParser from "body-parser";
+
+//..recursos para token JWT
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
+
+//..acesso as funcoes da aplicacao
+const productController = require('./Funcoes/fcProdutos');
+const groupController   = require('./Funcoes/fcGrupoProdutos');
+const userController    = require('./Funcoes/fcUsuarios');
+const tokenController   = require('./Funcoes/fcToken');
+
+//..aplicacao
+var cors = require('cors')
+const app = express();
+app.use(cors())
+
+//..utilizacao do Json
+var jsonParser = bodyParser.json()
+
+//..funcao para validar o token de acesso
+function verifyJWT(request, response, next){
+  const token = request.headers['x-access-token'];
+  if (!token) return response.status(401).json({ auth: false, message: 'No token provided.' });
+  
+  jwt.verify(token, process.env.SECRET, function(err, decoded) {
+    if (err) return response.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+    
+    // se tudo estiver ok, salva no request para uso posterior
+    request.userId = decoded.id;
+    next();
+  });
+};
+
 
 app.get("/", (request, response) => {
   return response.send("Utilize /produtos/consultar  ou /produtos/incluir");
 });
 
+//...ROTA GERACAO DE TOKEN
+app.post('/gerarToken', jsonParser, (request, response) => {
+    tokenController.gerarToken (request, response);
+});
+
+//...ROTAS PRODUTOS
 app.get("/produtos", (request, response) => {
-  return response.send("Utilize /produtos/consultar  ou /produtos/incluir");
+    return response.send("Utilize /produtos/consultar  ou /produtos/incluir");
 });
 
-app.get("/produto/consultar", (request, response) => {
-    //Request -- Entrada
-    //Responde -- Saída
-
-    con.query(`SELECT * FROM DataBases.cadastro`, function(err, result, fields) {
-            if (err) {
-              //con.end();
-              return response.send(err);
-             }
-            if (result) {
-              //con.end();
-              return response.send(result);
-            }
-    });
- 
+app.get("/produto/consultar", verifyJWT, (request, response) => {
+    productController.fcConsultarProd (request, response); 
 });
 
-app.post("/produto/incluir", jsonParser, (request, response) => {
-    //Request -- Entrada
-    //Responde -- Saída
-
-    con.query(`INSERT INTO DataBases.cadastro (cdProduto, nmProduto, qtEstoque, qtMinEstoque) VALUES (${request.body.cdProduto}, '${request.body.nmProduto}', ${request.body.qtEstoque} ,${request.body.qtMinEstoque})`, function(err, result, fields) {
-      if (err) {
-         //con.end();
-         return response.send(err);
-      }
-      if (result) {
-        //con.end();
-        return response.send(result);
-      }
-    });
-    
+app.get("/produto/consultarteste", verifyJWT, (request, response) => {
+    productController.fcConsultarProdteste (request, response); 
 });
 
-//http://localhost:3000
+app.get("/produto/consultar/:id", verifyJWT, (request, response) => {
+    productController.fcConsultarProdID (request, response);
+});
+
+app.post("/produto/incluir", jsonParser, verifyJWT, (request, response) => {
+    productController.fcIncluirProd (request, response);
+});
+
+//...ROTAS GRUPOS DE PRODUTOS
+app.get("/grupos", (request, response) => {
+    return response.send("Utilize /grupoproduto/consultar  ou /grupoproduto/incluir");
+});
+
+app.get("/grupoproduto/consultar", verifyJWT, (request, response) => {
+    groupController.fcConsultarGrupoProd (request, response); 
+});
+
+app.get("/grupoproduto/consultar/:id", verifyJWT, (request, response) => {
+    groupController.fcConsultarGrupoProdID (request, response);
+});
+
+app.post("/grupoproduto/incluir", jsonParser, verifyJWT, (request, response) => {
+    groupController.fcIncluirGrupoProd (request, response);
+});
+
+//...ROTAS USUARIOS
+app.get("/usuarios", (request, response) => {
+    return response.send("Utilize /usuarios/consultar  ou /usuarios/incluir");
+});
+
+app.get("/usuarios/consultar", verifyJWT, (request, response) => {
+    userController.fcConsultarUser (request, response); 
+});
+
+app.get("/usuarios/consultar/:email", verifyJWT, (request, response) => {
+    userController.fcConsultarUserID (request, response);
+});
+
+app.post("/usuarios/incluir", jsonParser, (request, response) => {
+    userController.fcIncluirUser (request, response);
+});
+
+//...APLICACAO - EXECUCAO
 app.listen( 3000, ()=> console.log("Server is Running"));
